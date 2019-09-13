0f2729f1e1cfe892584182a8c55111ac19cb5b0a
Reese Kunz <reesekunz21@gmail.com>
2019-09-13 08:43:52 -0700

    built out migrations to match data model

--- data/migrations/20190913082528_projects-table.js
+++ data/migrations/20190913082528_projects-table.js
@@ -1,8 +1,70 @@
-
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+      // Creating Projects Table
+      .createTable("projects", column => {
+        column.increments();
+        column
+          .text("project_name", 255)
+          .unique()
+          .notNullable();
+        column.text("project_description", 500);
+        column.boolean("completed");
+      })
+
+      // Creating Taks Table
+      .createTable("tasks", column => {
+        column.increments();
+        // Foreign key for project id
+        column
+          .integer("project_id")
+          .unsigned()
+          .references("id")
+          .inTable("projects")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+        column.text("description", 500).notNullable;
+        conlumn.text("notes", 500);
+        column.boolean("completed");
+      })
+
+      // Creating Resoures Table
+      .createTable("resources", column => {
+        column.increments();
+        column
+          .text("resource_name", 255)
+          .unique()
+          .notNullable();
+        column.text("resource_description", 500);
+      })
+
+      // Creating Project_Resources Table
+      .createTable("project_resources", column => {
+        column.increments();
+        // Foreign key for project id
+        column
+          .integer("project_id")
+          .unsigned()
+          .references("id")
+          .inTable("projects")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+        // Foreign key for resource id
+        column
+          .integer("resource_id")
+          .unsigned()
+          .references("id")
+          .inTable("resources")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
+      })
+  );
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableifExists("project_resources")
+    .dropTableifExists("resources")
+    .dropTableifExists("tasks")
+    .dropTableifExists("projects");
 };
